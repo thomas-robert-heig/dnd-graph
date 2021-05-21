531b705c965ea3517a5b110e645e0ab07728b471
Luka Najjar <lukanajjar@hotmail.com>
2021-05-21 16:37:15 +0200

    updated data preparation

--- src/index.js
+++ src/index.js
@@ -17,17 +17,28 @@ const CHARS = Object.entries(DATA);
 //     }))
 // );
 
+let recupererSpells = (spells) => {
+    return Object.keys(spells)
+                .map(key => spells[key].processedSpell)
+                .reduce((r, d) => [...r, d[0]], []);
+}
+
+let recupererWeapons = (weapons) => {
+    return Object.keys(weapons)
+                .map(key => weapons[key].processedWeapon)
+                .reduce((r, d) => [...r, d[0]], []);
+}
+
 const PREP_CHARS = CHARS
     .map(([nom, data]) => ({...data, nom}))
     .map(d => ({
         race: d?.race.processedRace[0],
-        class: d?.class.class,
+        class: Object.keys(d?.class)[0],
         skills: d?.skills,
-        spells: d?.spells,
+        spells: recupererSpells(d?.spells),
         feats: d?.feats,
-        weapons: d?.weapons,
+        weapons: recupererWeapons(d?.weapons),
         alignment: d.alignment.processedAlignment[0],
-        name: d.name.alias[0]
     }))
     .filter(d => 
         d.race == 'Dragonborn' ||
@@ -40,19 +51,19 @@ const PREP_CHARS = CHARS
         d.race == 'Human' ||
         d.race == 'Tiefling'
     )
-    // .filter(d => 
-    //     d.class == 'Barbarian' ||
-    //     d.class == 'Bard' ||
-    //     d.class == 'Cleric' ||
-    //     d.class == 'Druid' ||
-    //     d.class == 'Fighter' ||
-    //     d.class == 'Monk' ||
-    //     d.class == 'Paladin' ||
-    //     d.class == 'Ranger' ||
-    //     d.class == 'Rogue' ||
-    //     d.class == 'Sorcerer' ||
-    //     d.class == 'Warlock' ||
-    //     d.class == 'Wizard'
-    // )
+    .filter(d => 
+        d.class == 'Barbarian' ||
+        d.class == 'Bard' ||
+        d.class == 'Cleric' ||
+        d.class == 'Druid' ||
+        d.class == 'Fighter' ||
+        d.class == 'Monk' ||
+        d.class == 'Paladin' ||
+        d.class == 'Ranger' ||
+        d.class == 'Rogue' ||
+        d.class == 'Sorcerer' ||
+        d.class == 'Warlock' ||
+        d.class == 'Wizard'
+    )
 
 console.log(PREP_CHARS);
