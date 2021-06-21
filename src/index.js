a803ba40b4b0c4edf4cf9e081e8cdab27c088c14
Thomas Robert <thomas.robert@heig-vd.ch>
2021-06-21 19:24:48 +0200

    Add basic skill graph

--- src/index.js
+++ src/index.js
@@ -5,15 +5,5 @@
 /* Import & Settings
  ********************************/
 import { select, sum, scaleLinear, max } from "d3";
-import { DATA_SKILLS } from "./js/skills";
+import "./js/skills";
 import "./js/classRace";
-
-/* Define canvas
- ********************************/
-const canvas = select("#graph")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500)
-    .append("g")
-    .attr("transform", "translate(40,0)");
-
